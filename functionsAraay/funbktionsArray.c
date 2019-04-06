ef6a1828948e8e669d2eafceb0bdc3f519f61aba
Dan Li <akiraryo@hotmail.com>
2019-04-06 18:45:39 +0200

    function 一个array

---
+++ functionsAraay/funbktionsArray.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+void disArray(int* p, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", *p++);
+	}
+}
+
+
+int main() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	disArray(arr, sizeof(arr) / sizeof(*arr)); //*arr 等价于 arr[0]
+}
